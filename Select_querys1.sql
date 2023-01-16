0e4a7545cbdfe24b8f26f05b42ba19ffe58bf937
a.nikitina <nikitina_anna08@list.ru>
2023-01-16 11:40:16 +0300

    change 4 and 9 querys

--- Select_querys1.sql
+++ Select_querys1.sql
@@ -7,45 +7,50 @@ SELECT COUNT(production_year) FROM album a
 JOIN track t ON t.album_id = a.id
 WHERE production_year BETWEEN '2019-01-01' AND '2020-12-31';
 
-SELECT title, AVG(lenghs) from album a
+SELECT title, AVG(lenghs) FROM album a
 JOIN track t ON t.album_id = a.id
 GROUP by title
-ORDER BY COUNT(*) desc;
+ORDER BY COUNT(*) DESC;
 
-select name from performer p 
-join albumperformer a on p.id = a.id_performer 
-join album a2 on a.id_album = a2.id 
-where production_year not BETWEEN '2020-01-01' AND '2020-12-31';
+SELECT name FROM performer p 
+JOIN albumperformer a ON p.id = a.id_performer 
+JOIN album a2 ON a.id_album = a2.id 
+WHERE name NOT IN (SELECT name FROM performer p
+WHERE production_year BETWEEN '2020-01-01' AND '2020-12-31');
 
-select names, name from collection c 
-join collectiontrack c2 on c.id = c2.id_collection 
-join track t on c2.id_track = t.id 
-join album a on t.album_id = a.id 
-join albumperformer a2 on a.id = a2.id_album 
-join performer p on a2.id_performer = p.id 
-where name = 'Boney M'
+SELECT names, name FROM collection c 
+JOIN collectiontrack c2 ON c.id = c2.id_collection 
+JOIN track t ON c2.id_track = t.id 
+JOIN album a ON t.album_id = a.id 
+JOIN albumperformer a2 ON a.id = a2.id_album 
+JOIN performer p ON a2.id_performer = p.id 
+WHERE name = 'Boney M'
 
-select titles, genre_name from album a 
-join albumperformer a2 on a.id = a2.id_album 
-join performer p on a2.id_performer = p.id 
-join genreperformer g on p.id = g.id_performer 
-join genre g2 on g.id_genre = g2.id
+SELECT titles, genre_name FROM album a 
+JOIN albumperformer a2 ON a.id = a2.id_album 
+JOIN performer p ON a2.id_performer = p.id 
+JOIN genreperformer g ON p.id = g.id_performer 
+JOIN genre g2 ON g.id_genre = g2.id
 GROUP BY genre_name, titles
-having COUNT(*) > 1;
+HAVING COUNT(*) > 1;
 
-select title from track t 
-left Join collectiontrack c on t.id = c.id_track 
-where c.id_track is  NULL 
+SELECT title FROM track t 
+LEFT JOIN collectiontrack c ON t.id = c.id_track 
+WHERE c.id_track IS  NULL 
 
-select name, lenghs from performer p 
-join albumperformer a on p.id = a.id_performer 
-join album a2 on a.id_album = a2.id 
-join track t on a2.id = t.album_id 
+SELECT name, lenghs FROM performer p 
+JOIN albumperformer a ON p.id = a.id_performer 
+JOIN album a2 ON a.id_album = a2.id 
+JOIN track t ON a2.id = t.album_id 
 WHERE lenghs = (SELECT MIN(lenghs) FROM track)
 
-select titles, COUNT(*) as c from album a 
-left join track t on a.id = t.album_id 
+SELECT titles FROM album a 
+LEFT JOIN track t ON a.id = t.album_id 
 GROUP by titles
-ORDER BY c ASC
-limit 1
+HAVING count(title) =
+(SELECT count(title) FROM album a 
+LEFT JOIN track t ON a.id = t.album_id 
+GROUP by titles
+ORDER BY count(title) ASC
+LIMIT 1)
 
